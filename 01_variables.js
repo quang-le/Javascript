ae578205c0f5a8fc2f266cd18fcde8fc7dbbf14a
quang-le <quangle.bxl@gmail.com>
2018-05-30 16:46:10 +0200

    add 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+var profession="Necromancer", country="Middle Earth", name="Sauron", children=9;
+console.log('vous serez', profession, `et habiterez à ${country}, et marié à ${name} avec ${children} enfants`);
+
+var thisYear=2018, birthYear=1980;
+console.log(thisYear-birthYear);
+
+
