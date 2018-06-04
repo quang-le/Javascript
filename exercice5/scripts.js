ad58def02f60376f4af5c96b4d3c5c46aec21f57
quang-le <quangle.bxl@gmail.com>
2018-06-04 16:57:14 +0200

    end of day commit

---
+++ exercice5/scripts.js
@@ -0,0 +1,12 @@
+float1= document.getElementById("premier_nombre").value;
+float2= document.getElementById("deuxieme_nombre").value;
+
+function multiply (integer, float){
+    integer=Math.floor(float1);
+    float=float2;
+    alert(integer*float);
+    console.log(float1);
+    console.log(float2);
+}
+
+
