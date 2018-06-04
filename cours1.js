36aa1e1606ff01e235b4a0a44e0e64b5655cc777
quang-le <quangle.bxl@gmail.com>
2018-06-04 16:14:47 +0200

    exos 1 à 4

---
+++ cours1.js
@@ -0,0 +1,28 @@
+
+//challenge 1 Intro
+var age=23;
+alert(age);
+userAge=prompt("Quel est ton age?");
+alert(userAge);
+ageSoeur=age+11;
+age+=12;
+alert(age);
+// Challenge 2 Intro
+name=prompt("What is your name?", "2 more questions")
+quest=prompt("what is your quest?", "1 more question");
+color=prompt ("what is your favorite color?", "last question");
+alert("Welcome,  "+name+ ". Good luck on this quest to "+quest+ ". OMG, "+color+" what a shitty choice!");
+
+//Exercice 1 Parcours
+var troll="lol";
+alert(troll);
+
+//Exercice 2 parcours
+var firstName="John";
+var lastName="Doe";
+var city="New York";
+alert("Nom: "+ lastName+ "\nPrénom: "+firstName+"\nVille: "+city);
+
+//Exercice 3 parcours
+userName=prompt("What is your name?");
+alert("Bonjour, "+userName);
---
+++ exercice4/scripts.js
@@ -0,0 +1,18 @@
+/*var nomForm= document.getElementById('nom');
+var nom= nomForm.value;
+var prenomForm=document.getElementById('prenom');
+var prenom=prenomForm.value;
+var villeForm= document.getElementById('ville');
+var ville= villeForm.value;*/
+
+
+function formSubmit(nom, prenom, ville){
+    nom=document.getElementById('nom').value;
+    prenom= document.getElementById('prenom').value;
+    ville= document.getElementById('ville').value;
+    alert("Nom: "+ nom+ "\nPrénom: "+prenom+"\nVille: "+ville);
+
+}
+
+
+
