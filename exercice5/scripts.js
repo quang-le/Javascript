0292d2f1ea67aa4edc4b3d4da4bdbfe2e83f2eee
quang-le <quangle.bxl@gmail.com>
2018-06-07 09:43:34 +0200

    exercice6

--- exercice5/scripts.js
+++ exercice5/scripts.js
@@ -1,13 +1,17 @@
 
-
+/*exercice5
 function multiply (integer, float){
     integer= document.getElementById("premier_nombre").value;
     float= document.getElementById("deuxieme_nombre").value;
     integer=Math.floor(integer);
     alert(integer*float);
-   /* console.log(float1);
-    console.log(float2);*/
 }
-/*multiply(float1,float2)*/
+/*exercice 6*/
+const modulo=(integer,float)=> {
+    integer= document.getElementById("premier_nombre").value;
+    float= document.getElementById("deuxieme_nombre").value;
+    integer=Math.floor(integer);
+    alert(integer%float);
+}
 
 
