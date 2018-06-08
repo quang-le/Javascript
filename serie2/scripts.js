4ab75e38899b2a12a560f4984f5b9128a29af0cc
quang-le <quangle.bxl@gmail.com>
2018-06-08 11:43:57 +0200

    serie 2 exo 4

--- serie2/scripts.js
+++ serie2/scripts.js
@@ -35,3 +35,12 @@ let affiche = ()=> {
     alert(triple(a));
     alert(triple(b));
 }
+
+/*exercice4*/
+var tab=[-2,1,4];
+let additionne = x => {return x+2};
+
+let affiche2= ()=>{
+    alert(additionne(tab[0]));
+    alert(additionne(tab[2]));
+}
