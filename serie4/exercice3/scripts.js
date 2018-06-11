7be4893b88c4bc1df75109a0958e859e7cfe8455
quang-le <quangle.bxl@gmail.com>
2018-06-11 15:18:32 +0200

    exo4-3

---
+++ serie4/exercice3/scripts.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("red")[0].onclick=()=> document.getElementById("texte").setAttribute("style", "color:red");
+document.getElementsByClassName("green")[0].onclick=()=> document.getElementById("texte").setAttribute("style", "color:green");
+document.getElementsByClassName("blue")[0].onclick=()=> document.getElementById("texte").setAttribute("style", "color:blue");
