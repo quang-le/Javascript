e5433a945d80f1f4b9b97be3f85e5e7bc9214fe8
quang-le <quangle.bxl@gmail.com>
2018-06-11 17:19:07 +0200

    setup serie6

---
+++ serie6/questions.js
@@ -0,0 +1,43 @@
+var CreationTableauLangages = function () {
+
+}
+
+var CreationTableauNombres = function () {
+
+}
+
+var RemplacementElement = function (langages) {
+
+}
+
+var AjoutElementLangages = function (langages) {
+
+}
+
+var AjoutElementNombres = function (nombres) {
+
+}
+
+var SuppressionPremierElement = function (langages) {
+
+}
+
+var SuppressionDernierElement = function (langages) {
+
+}
+
+var ConversionChaineTableau = function (reseaux_sociaux_chaine) {
+
+}
+
+var ConversionTableauChaine = function (langages) {
+
+}
+
+var TriTableau = function (reseaux_sociaux) {
+
+}
+
+var InversionTableau = function (reseaux_sociaux){
+
+}
