9868a6c589b0427186292361af25488ced1bb20b
quang-le <quangle.bxl@gmail.com>
2018-06-14 22:52:49 +0200

    chop into smaller functions

--- pendu/main.js
+++ pendu/main.js
@@ -1,7 +1,7 @@
 
 
 guessLetter=()=>{
-
+    //Variables
     var solution=["B","O","N","J","O","U","R"]; // la solution
     var guesses=[];                             //stocke les tentatives du joueur
     var answer=["_","_","_","_","_","_","_"];   //sert à afficher les lettres devinées
@@ -12,12 +12,16 @@ guessLetter=()=>{
     let printGuesses=document.createElement("P");       //affiche les lettres déjà essayées
     let guessesText=document.createTextNode(guesses.reverse().join("-"));
     
-
+    //Functions
+    //messing around with DOM
     generateHTML=(tag,content)=> {
         tag.appendChild(content);
         document.body.appendChild(tag)
-    } 
+    }
+    //Update guesses and answers 
     updateHTML=(where,what)=> where.innerHTML=what.join();
+    
+    //Compares letters
     compareLetters=()=>{
         guess=prompt("Entrez une lettre");
         guesses.push(guess);
@@ -30,12 +34,20 @@ guessLetter=()=>{
             }
         }   
     }
-
-    generateHTML(printAnswer,answerText);
-    generateHTML(printGuesses,guessesText);
-
+    //Check if word is complete
+    checkComplete = ()=>{
+        if(solution.join()==answer.join()){
+            return alert("Félicitations, la solution était "+answer.join());
+        }
+        else{
+            alert("Essayez encore");
+            playGame();
+        }
+    }
     
+    //The actual game sequence
     playGame=()=>{
+        
         compareLetters();
 
         console.log(guesses);
@@ -43,16 +55,13 @@ guessLetter=()=>{
         // affiche les arrays mis à jour
         updateHTML(printAnswer,answer);
         updateHTML(printGuesses,guesses);
-
-
-        if(solution.join()==answer.join()){
-            return alert("Félicitations, la solution était "+answer.join());
-        }
-        else{
-            alert("Essayez encore");
-            playGame();
-        }    
+        checkComplete();
     }
+
+    //Run
+    generateHTML(printAnswer,answerText);
+    generateHTML(printGuesses,guessesText);
+
     playGame();
 }
 window.onload=()=> guessLetter();
