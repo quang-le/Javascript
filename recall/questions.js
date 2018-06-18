57c54af60febc747b4280b731c487301ec1485dc
quang-le <quangle.bxl@gmail.com>
2018-06-18 17:00:00 +0200

    end of day commit

--- recall/questions.js
+++ recall/questions.js
@@ -1,77 +1,130 @@
 var selectElementsStartingWithA = function(array) {
-    return 'Write your method here';
+    
+    return array.filter(function(item){
+       return item.startsWith("a")==true});
 }
 
 var selectElementsStartingWithVowel = function(array) {
-    return 'Write your method here';
+    return array.filter(function(item){
+        if(item.startsWith("a")==true || item.startsWith("i")==true || item.startsWith("e")==true || item.startsWith("o")==true || item.startsWith("u")==true){
+            return item;
+        }
+    })
 }
 
 var removeNullElements = function(array) {
-    return 'Write your method here';
+    return array.filter(function(item){
+        
+        return item!=null;
+        })
+    
 }
 
 var removeNullAndFalseElements = function(array) {
-    return 'Write your method here';
-}
+    console.log(array);
+    return array.filter(function(item){
+            return(typeof item==="string" || typeof item=="number")
+    })
+};
+
 
 var reverseWordsInArray = function(array) {
-    return 'Write your method here';
+        for(i=0;i<array.length;i++){
+           array[i]=(array[i].split("").reverse().join("")); 
+         }
+         return array;       
 }
 
 var everyPossiblePair = function(array) {
-    return 'Write your method here';
-}
+    let array2=[]
+    array.sort(); //met les éléments dans le bon ordre
+    console.log(array2);
+    for (i=0;i<array.length;i++){
+        for (j=i+1;j<array.length;j++){ // j+1 pour faire le loop sur l'élément suivant, array.length pour empêcher undefined
+        array2.push([array[i],array[j]]); //pq array2[i]=[array[i],array[j]] loop que 2 fois?? PARCE QU'IL REMPLACE LA PREMIERE PAIRE GENEREE PAR LA DEUXIEME!!
+        }
+    }
+    console.log(array2);
+    console.log(typeof array2[0]);
+    return array=array2;
+    }
 
 var allElementsExceptFirstThree = function(array) {
-    return 'Write your method here';
+    return array.slice(3,array.length);
 }
 
 var addElementToBeginning = function(array, element) {
-    return 'Write your method here';
+    array.splice(0,0,element);
+    return array;
 }
 
 var sortByLastLetter = function(array) {
-    return 'Write your method here';
+    
+    return array.sort(function(a,b){
+        return a.charCodeAt(a.length-1)-b.charCodeAt(b.length-1)
+    })
 }
 
 var getFirstHalf = function(string) {
-    return 'Write your method here';
+    return string.slice(0,(string.length+1)/2);
 }
 
 var makeNegative = function(number) {
-    return 'Write your method here';
+    return -Math.abs(number);
 }
 
 var numberOfPalindromes = function(array) {
-    return 'Write your method here';
+    
+    return array.filter(function (e){
+       return e==e.split("").reverse().join("");
+
+    }).length;
 }
 
 var shortestWord = function(array) {
-    return 'Write your method here';
+    array.sort(function(a,b){
+        return a.length-b.length;
+    })
+    return array[0];
 }
 
 var longestWord = function(array) {
-    return 'Write your method here';
+    array.sort(function(a,b){
+        return a.length-b.length;
+    })
+    return array[array.length-1];
 }
 
+
 var sumNumbers = function(array) {
-    return 'Write your method here';
+
+    return array.reduce(function(total,num){ //total agrège les résultats, num= l'élément suivant à être ajouté
+        return total+num;
+    });
 }
 
 var repeatElements = function(array) {
-    return 'Write your method here';
+    array.forEach(function(e){
+         array.push(e);
+         console.log(array)
+        
+    })
+    return array;
 }
 
 var stringToNumber = function(string) {
-    return 'Write your method here';
+    return Number(string);
 }
 
 var calculateAverage = function(array) {
-    return 'Write your method here';
-}
+    return (array.reduce(function(total,num){ //total agrège les résultats, num= l'élément suivant à être ajouté
+        return total+num;}))/array.length;
+    };
 
 var getElementsUntilGreaterThanFive = function(array) {
-    return 'Write your method here';
+    return array.filter(function(e){
+       return array.indexOf(e)<=6; //indexOf doesn't eliminate
+    })
 }
 
 var convertArrayToObject = function(array) {
